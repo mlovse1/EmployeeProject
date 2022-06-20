97c8630e71f4355914acc289e657c62bd5f696cb
mlovse1 <67603844+mlovse1@users.noreply.github.com>
2022-06-20 15:58:36 -0500

    Added Comments

--- exceptions/IdExistsException.java
+++ exceptions/IdExistsException.java
@@ -1,4 +1,5 @@
 package com.misha.jump.exceptions;
+//exception when attempting to add employee with same id numbers
 
 public class IdExistsException extends Exception {
 private static final long serialVersionUID = 3894890100607995608L;
--- exceptions/YesorNoException.java
+++ exceptions/YesorNoException.java
@@ -1,5 +1,7 @@
 package com.misha.jump.exceptions;
 
+//Exception that is thrown if a user tries to enter an acceptance other than Y or N 
+
 public class YesorNoException extends Exception {
 	private static final long serialVersionUID = 9098578360011113179L;
 
