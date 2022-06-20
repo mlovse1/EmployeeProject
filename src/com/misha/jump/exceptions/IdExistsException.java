7b1ad6d766ade4a0afa5ed424833ce435be185ef
mlovse1 <67603844+mlovse1@users.noreply.github.com>
2022-06-20 15:20:53 -0500

    Delete src/com/misha/jump directory

--- src/com/misha/jump/exceptions/IdExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.misha.jump.exceptions;
-
-public class IdExistsException extends Exception {
-private static final long serialVersionUID = 3894890100607995608L;
-	
-	public IdExistsException(int id) {
-		super("An employee with ID: "+id+" already exists.");
-	}
-}
--- src/com/misha/jump/exceptions/YesorNoException.java
+++
@@ -1,9 +0,0 @@
-package com.misha.jump.exceptions;
-
-public class YesorNoException extends Exception {
-	private static final long serialVersionUID = 9098578360011113179L;
-
-	public YesorNoException(){
-		super("Invalid entry! Enter Y or N!");
-	}
-}
